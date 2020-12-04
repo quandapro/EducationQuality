4c60d9c21e6355820a4096f96f4ca40bb2a6b0a9
Hieud1999 <doxuanhieu_t62@hus.edu.vn>
2020-12-04 11:35:38 +0700

    Add features of table chuongtrinhdaotao

---
+++ app/ChuongTrinhDaoTao.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class ChuongTrinhDaoTao extends Model
+{
+    protected $table = 'chuongtrinhdaotao';
+    public $timestamps = false;
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array
+     */
+    protected $fillable = [
+        'id_CTDT', 'ten_CTDT','id_nguoiKD', 'ngay_kd', 'ngayHetHan_kd', 'thoiHan', 'vanban_kd'
+    ];
+}
---
+++ app/Http/Controllers/CTDTController.php
@@ -0,0 +1,110 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ChuongTrinhDaoTao;
+use Illuminate\Http\Request;
+use DB;
+
+
+class CTDTController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $programs = DB::select('select * from chuongtrinhdaotao');
+        return view('chuongtrinhdaotao.index', compact('programs'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('chuongtrinhdaotao.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        $data = $request->input();
+        try {
+            $program = new ChuongTrinhDaoTao;
+            $program->ten_CTDT = $data['ten_CTDT'];
+            $program->ngay_kd = $data['ngay_kd'];
+            $program->ngayHetHan_kd = $data['ngayHetHan_kd'];
+            $program->thoiHan = $data['thoiHan'];
+            $program->vanBan_kd = $data['vanBan_kd'];
+            $program->save();
+            return redirect('/view_CTDT_programs')->with('status', "Insert successfully");
+        }
+        catch(Exception $e){
+            return redirect('/view_CTDT_programs')->with('failed',"operation failed");
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ChuongTrinhDaoTao  $chuongTrinhDaoTao
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        $programs = DB::select ('select * from chuongtrinhdaotao where id_CTDT = ?', [$id]);
+        //dd($programs);
+        return  view('chuongtrinhdaotao.update', compact('programs'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ChuongTrinhDaoTao  $chuongTrinhDaoTao
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ChuongTrinhDaoTao $chuongTrinhDaoTao)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ChuongTrinhDaoTao  $chuongTrinhDaoTao
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        $ten_CTDT = $request -> input('ten_CTDT');
+        $ngay_kd = $request -> input('ngay_kd');
+        $ngayHetHan_kd = $request -> input('ngayHetHan_kd');
+        $thoiHan = $request -> input('thoiHan');
+        $vanBan_kd = $request -> input('vanBan_kd');
+        DB::update('update chuongtrinhdaotao set ten_CTDT = ?, ngay_kd=?, ngayHetHan_kd=?,thoiHan=?, vanBan_kd = ? where id_CTDT = ?', [$ten_CTDT,$ngay_kd,$ngayHetHan_kd,$thoiHan,$vanBan_kd, $id]);
+        return redirect('/view_CTDT_programs')->with('status', "Insert successfully");
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ChuongTrinhDaoTao  $chuongTrinhDaoTao
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        DB::table('chuongtrinhdaotao')->where('id_CTDT', '=', $id)->delete();
+        return redirect('/view_CTDT_programs')->with('status', "Insert successfully");
+    }
+}
