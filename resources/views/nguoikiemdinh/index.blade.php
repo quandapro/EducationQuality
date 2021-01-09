17833d49715134c69e2e1b39abd0a70bb6048bd6
hanhpham99 <hanhphamhus@gmail.com>
2021-01-09 20:32:29 +0700

    edit giao diện người kiểm định 09/01

--- resources/views/nguoikiemdinh/index.blade.php
+++ resources/views/nguoikiemdinh/index.blade.php
@@ -17,35 +17,197 @@
 </head>
 
 <body>
-    <h1>Danh sách người kiểm định</h1>
-    <table class="table table-dark">
-    <thead>
-        <tr>
-        <th scope="col">Mã số</th>
-        <th scope="col">Họ tên</th>
-        <th scope="col">Ngày sinh</th>
-        <th scope="col">Quê quán</th>
-        <th scope="col">Đơn vị kiểm định</th>
-        <th scope="col"></th>
-        </tr>
-    </thead>
-    <tbody>
-    @foreach ($inspections as $inspection)
-        <tr>
-        <td>{{ $inspection->id_nguoiKD }}</td>
-        <td>{{ $inspection->full_name }}</td>
-        <td>{{ $inspection->ngaySinh }}</td>
-        <td>{{ $inspection->queQuan }}</td>
-        <td>{{ $inspection->id_DVKD }}</td>
-        <td>
-            <a href="/update-inspection/{{ $inspection->id_nguoiKD }}" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Sửa</a>
-            <a href="/destroy-inspection/{{ $inspection->id_nguoiKD }}" class="btn btn-secondary btn-lg active" role="button" aria-pressed="true">Xóa</a>
-        </td>
-        </tr>
-    @endforeach
-    </tbody>
-    </table>
-    <button type="button" class="btn btn-success">Thêm mới</button>
+    <div id="header" class="container-fluid">
+        <div class="row">
+            <div class="col-sm-6 col-md-10">
+                <div class="col-md-3 col-sm-12 logo-home" id="logo-home">
+                    <a href="home.html"><img class="img-responsive" style="width:200px; height: 65px;" src="images/logo.jpg" alt="Chania"></a>
+                </div>
+                <div class="col-md-9 col-sm-12 menu-home">
+                    <nav class="navbar navbar-expand-lg">
+                        <a class="nav-link" href="#">Trang chủ <span class="sr-only">(current)</span></a>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                          <span class="navbar-toggler-icon"></span>
+                        </button>
+
+                        <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                            <ul class="navbar-nav mr-auto">
+                                <li class="nav-item active">
+                                    <a class="nav-link" href="page_intro.html">Giới thiệu <span class="sr-only">(current)</span></a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="#">Sự kiện</a>
+                                </li>
+                                <li class="nav-item dropdown">
+                                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Kết quả KDCLGD
+                              </a>
+                                    <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                        <a class="dropdown-item" href="#">Kiểm định các cấp</a>
+                                        <a class="dropdown-item" href="#">Kiểm định chương trình đào tạo</a>
+                                        <div class="dropdown-divider"></div>
+                                        <a class="dropdown-item" href="#">Kiểm định chứng chỉ</a>
+                                    </div>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link disabled" href="#">Liên hệ</a>
+                                </li>
+                            </ul>
+                            <form class="form-inline my-2 my-lg-0">
+                                <input class="form-control mr-sm-2" type="search" aria-label="Search">
+                                <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Tìm kiếm</button>
+                            </form>
+                        </div>
+                    </nav>
+                </div>
+            </div>
+            <div class="col-sm-6 col-md-2 header-login-signup">
+                <button type="button" class="btn btn-primary btn-login" id="btn-login"><a href="page_home.html">Đăng xuất</a></button>
+            </div>
+        </div>
+    </div>
+    <div id="body" class="container">
+        <div class="row slide-body">
+            <div class="bd-example">
+                <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <img src="images/variant1.jpg" class="img-thumbnail d-block " alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Kiểm định chất lượng giáo dục</h5>
+                                <p>website quản lý kết quả kiểm định chất lượng giáo dục</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="images/variant2.jpg" class="img-thumbnail d-block " alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Kiểm định chất lượng giáo dục</h5>
+                                <p>website quản lý kết quả kiểm định chất lượng giáo dục</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="images/variant3.jpg" class="img-thumbnail d-block " alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Kiểm định chất lượng giáo dục</h5>
+                                <p>Website quản lý kết quả kiểm định chất lượng giáo dục</p>
+                            </div>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleCaptions" role="button" data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleCaptions" role="button" data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div>
+        </div>
+
+        <div class="container partner-body">
+            <div class="row">
+                <div class="col-md-12">
+                    <div class="list-partner">
+                        <a href="#">
+                            <img src="images/partner1.jpg" class="img-thumbnail" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="images/partner4.jpg" class="img-thumbnail" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="images/logo1.jpg" class="img-thumbnail" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="images/partner2.jpg" class="img-thumbnail" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="images/partner6.jpg" class="img-thumbnail" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="images/partner3.jpg" class="img-thumbnail" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="images/partner5.jpg" class="img-thumbnail" alt="">
+                        </a>
+                        <a href="#">
+                            <img src="images/partner7.jpg" class="img-thumbnail" alt="">
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+        <div class="container infomation-body">
+            <div class="row infomation-body-title">
+                <h3 style="color: #2a73cc;">Danh sách người kiểm định</h3>
+            </div>
+            <div class="row left-info">
+                <table border="2" class="table table-hover">
+                    <thead>
+                        <tr>
+                        <th scope="col">Mã số</th>
+                        <th scope="col">Họ tên</th>
+                        <th scope="col">Ngày sinh</th>
+                        <th scope="col">Quê quán</th>
+                        <th scope="col">Đơn vị kiểm định</th>
+                        <th scope="col"></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach ($inspections as $inspection)
+                        <tr>
+                            <td>{{ $inspection->id_nguoiKD }}</td>
+                            <td>{{ $inspection->full_name }}</td>
+                            <td>{{ $inspection->ngaySinh }}</td>
+                            <td>{{ $inspection->queQuan }}</td>
+                            <td>{{ $inspection->id_DVKD }}</td>
+                            <td id="setting">
+                                <a href="/update-inspection/{{ $inspection->id_nguoiKD }}" class="btn btn-primary active" role="button" aria-pressed="true">Sửa</a>
+                                <a href="/destroy-inspection/{{ $inspection->id_nguoiKD }}" class="btn btn-danger active" role="button" aria-pressed="true">Xóa</a>
+                            </td>
+                        </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+                <button type="button" class="btn btn-success">Thêm mới</button>
+            </div>
+        </div>
+    </div>
+    <div id="footer" class="container-fluid footer">
+        <div class="row">
+            <div class="col-md-5 footer-left">
+                <h3>Kiểm định CLGD</h3>
+                <div class="footer-intro">
+                    <p>
+                        Trang web được xây dựng với mục đích lưu trữ, truy xuất các thông tin liên quan đến kiếm định chất lượng giáo dục đối với các cơ sở giáo dục và chương trình đào tạo
+                    </p>
+                    <span> @20120 HUS University. All rights reserved.</span>
+                </div>
+            </div>
+            <div class="col-md-6 footer-more">
+                <div class="col-md-8 footer-more-item">
+                    <h5>Liên hệ</h5>
+                    <ul>
+                        <li>
+                            <p>Tổng đài: (+84-28) 37 242 160 - Số nội bộ: 4994, 4988</p>
+                        </li>
+                        <li>
+                            <p>ĐT trực tiếp/Fax: (+84-28) 62 849 199</p>
+                        </li>
+                        <li>
+                            <p>Email: ttkiemdinh@hus.edu.vn</p>
+                        </li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 
-</html>
+</html> 
