de5d1463c98f859913a473f515549bc99c03440e
hanhpham99 <hanhphamhus@gmail.com>
2021-01-09 20:55:30 +0700

    fix bug header

--- resources/views/nguoikiemdinh/update.blade.php
+++ resources/views/nguoikiemdinh/update.blade.php
@@ -23,43 +23,51 @@
 </head>
 
 <body>
-    <div id="header" class="container-fluid">
+   <div id="header" class="container-fluid">
         <div class="row">
             <div class="col-sm-6 col-md-10">
                 <div class="col-md-3 col-sm-12 logo-home" id="logo-home">
-                    <a href="home.html"><img class="img-responsive" style="width:200px; height: 65px;" src="images/logo.jpg" alt="Chania"></a>
+                    <a href="welcome"><img class="img-responsive" style="width:200px; height: 65px;" src="images/logo.jpg" alt="Chania"></a>
                 </div>
                 <div class="col-md-9 col-sm-12 menu-home">
                     <nav class="navbar navbar-expand-lg">
-                        <a class="nav-link" href="#">Trang chủ <span class="sr-only">(current)</span></a>
+                        <a class="nav-link" href="welcome">Trang chủ <span class="sr-only">(current)</span></a>
                         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                           <span class="navbar-toggler-icon"></span>
                         </button>
-
+                        <div class="card-body">
+                            @isset($role)
+                                @if ( $role == 0 )
+                                    <a href="/admin_home">Admin</a><br>
+                                @endif
+                            @endisset
+                            
+                        </div>
                         <div class="collapse navbar-collapse" id="navbarSupportedContent">
                             <ul class="navbar-nav mr-auto">
                                 <li class="nav-item active">
-                                    <a class="nav-link" href="page_intro.html">Giới thiệu <span class="sr-only">(current)</span></a>
+                                    <a class="nav-link" href="page_intro">Giới thiệu <span class="sr-only">(current)</span></a>
                                 </li>
                                 <li class="nav-item">
-                                    <a class="nav-link" href="#">Sự kiện</a>
+                                    <a class="nav-link" href="welcome">Sự kiện</a>
+                                </li>
+                                <li class="nav-item">
+                                    <a class="nav-link" href="/nguoikiemdinh-records">Danh sách người kiểm định</a>
                                 </li>
                                 <li class="nav-item dropdown">
                                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                                 Kết quả KDCLGD
                               </a>
                                     <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="#">Kiểm định các cấp</a>
-                                        <a class="dropdown-item" href="#">Kiểm định chương trình đào tạo</a>
-                                        <div class="dropdown-divider"></div>
-                                        <a class="dropdown-item" href="#">Kiểm định chứng chỉ</a>
+                                        <a href="/view_CSGD_programs" class="dropdown-item" href="#">Kiểm định cơ sở giáo dục</a>
+                                        <a href="/view_CTDT_programs" class="dropdown-item" href="#">Kiểm định chương trình đào tạo </a>
                                     </div>
                                 </li>
                                 <li class="nav-item">
                                     <a class="nav-link disabled" href="#">Liên hệ</a>
                                 </li>
                             </ul>
-                            <form class="form-inline my-2 my-lg-0">
+                            <form class="form-inline my-2 my-lg-0" action="page_quaility_item">
                                 <input class="form-control mr-sm-2" type="search" aria-label="Search">
                                 <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Tìm kiếm</button>
                             </form>
@@ -68,7 +76,7 @@
                 </div>
             </div>
             <div class="col-sm-6 col-md-2 header-login-signup">
-                <button type="button" class="btn btn-primary btn-login" id="btn-login"><a href="page_home.html">Đăng xuất</a></button>
+                <button type="button" class="btn btn-primary btn-login" id="btn-login"><a href="/logout">Đăng xuất</a></button>
             </div>
         </div>
     </div>
